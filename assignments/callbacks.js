be2301d7151b87cfa7dc1cdd5de58e4202d576fd
unknown <ch04937>
2019-06-11 13:53:06 -0700

    multiply nums -callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -54,7 +54,12 @@ sumNums(1,2, function(sum){
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb (x*y);
 }
+multiplyNums(3,3, function(multiply){
+  console.log(multiply);
+});
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
